7ca37b01add15e7f09492244c3774fa403501d20
TTPBoss <64545935+TTPBoss@users.noreply.github.com>
2023-06-22 01:18:45 -0700

    Im sleepy so just commit changes and will work on it tomorrow

--- main.cpp
+++ main.cpp
@@ -8,6 +8,26 @@ int main(int argc, char *argv[]) {
     QFile file(":/darktheme.qss");
     if (file.open(QFile::ReadOnly)) {
         QString styleSheet = QLatin1String(file.readAll());
+
+        styleSheet += "QScrollBar:horizontal {"
+                      "border: none;"
+                      "background: #282828;"
+                      "height: 10px;"
+                      "margin: 0px 0px 0px 0px;"
+                      "border-radius: 5px;"
+                      "}"
+                      "QScrollBar::handle:horizontal {"
+                      "background: #888;"
+                      "min-width: 20px;"
+                      "border-radius: 5px;"
+                      "}"
+                      "QScrollBar::add-line:horizontal, QScrollBar::sub-line:horizontal {"
+                      "border: none;"
+                      "background: none;"
+                      "}"
+                      "QScrollBar::add-page:horizontal, QScrollBar::sub-page:horizontal {"
+                      "background: none;"
+                      "}";
         app.setStyleSheet(styleSheet);
     } else {
         // Log error or notify the user here
@@ -17,3 +37,4 @@ int main(int argc, char *argv[]) {
     mainWindow.show();
     return QApplication::exec();
 }
+
